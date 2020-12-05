91fb91e23d43614375e48bf1cc99b52ef2b91e6f
paulicollantes <pfcollantes@uc.cl>
2020-12-05 09:52:30 -0300

    Formulas

---
+++ formulas.py
@@ -0,0 +1,29 @@
+import math 
+
+r_esfera = 2
+a_cubo = 2
+b_prisma = 2
+a_prisma = 2
+l_prisma = 2
+
+
+
+#Densidades fluidos
+d_agua = 998
+d_aceite = 933.7
+d_glicerina = 1260
+d_etanol = 789
+
+#Densidades materiales
+d_madera = 450
+d_aluminio = 2700
+d_hielo = 916,8
+d_corcho = 240
+
+#Volumen figuras
+v_esfera = 4*math.pi*r_esfera**3/3
+v_cubo = a_cubo**3
+v_prismat = b_prisma*a_prisma*l_prisma/2
+
+#Volumen carena
+
